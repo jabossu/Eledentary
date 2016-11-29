fff9f7e1d37801e43bfe4a5330128556c2998512
Jacques-Alexandre Bossu <ja.bossu@cmcluj.fr>
2016-11-29 09:56:31 +0200

    Fixed : configuration doesnt reset on page reload anymore 	modified:   classes/siteconfig.class.php 	modified:   classes/siteconfigManager.class.php

--- pages/reflecteurs/m_config.php
+++ pages/reflecteurs/m_config.php
@@ -1,15 +1,11 @@
 <?php
-$c = new siteconfig( array() );
 $cm = new siteconfigManager( $bdd ) ;
 
-$old = $cm->get() ;
+$old = $cm->get() ; 
 $new = new siteconfig( $_POST );
-
-//show( $cm->merge( $old, $new ) ) ;
 
 $siteconfig = $cm->merge( $old, $new );
 $cm->update( $siteconfig );
-$siteconfig = $cm->get();
 
 
 
