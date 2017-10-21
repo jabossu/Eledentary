16963f41a1410d9cee425d3e5cca578e30eeb542
Jacques-A Bossu <ja.bossu@gmail.com>
2017-10-21 17:27:17 +0300

    FIXED : Updating patients changed their status

--- pages/statiques/s_diagnosys.php
+++ pages/statiques/s_diagnosys.php
@@ -6,5 +6,16 @@ echo '<h2>$_SESSION</h2>';
 print_r($_SESSION);
 
 
+echo ($_POST == array() ) ?1 :0  ;
+
 ?>
 </pre>
+
+<?php
+
+$f = new form('classic', "boom" ) ;
+$f->input('nom', "test", 'text', "hello", 'Name' );
+$f->submit( translate('register') ) ; # submit button
+$f->output() ;
+
+
